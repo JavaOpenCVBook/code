1de24151b2c3f3cd16106975d5e80188b292788a
Daniel Lélis Baggio <danielbaggio@gmail.com>
2014-01-15 03:15:59 -0200

    Correcting toBufferedImage method
    It wasn't that fast.

--- chapter2/swing-imageshow/src/main/java/org/javaopencvbook/util/ImageViewer.java
+++ chapter2/swing-imageshow/src/main/java/org/javaopencvbook/util/ImageViewer.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -61,20 +62,18 @@ public class ImageViewer {
 		}
 	}
 	
-	public Image toBufferedImage(Mat m){
+	public Image toBufferedImage(Mat matrix){
 		int type = BufferedImage.TYPE_BYTE_GRAY;
-		if ( m.channels() > 1 ) {
-			Mat m2 = new Mat();
-			Imgproc.cvtColor(m,m2,Imgproc.COLOR_BGR2RGB);
+		if ( matrix.channels() > 1 ) {
 			type = BufferedImage.TYPE_3BYTE_BGR;
-			m = m2;
 		}
-		byte [] b = new byte[m.channels()*m.cols()*m.rows()];
-		m.get(0,0,b); // get all the pixels
-		BufferedImage image = new BufferedImage(m.cols(),m.rows(), type);
-		image.getRaster().setDataElements(0, 0, m.cols(),m.rows(), b);
+		int bufferSize = matrix.channels()*matrix.cols()*matrix.rows();
+		byte [] buffer = new byte[bufferSize];
+		matrix.get(0,0,buffer); // get all the pixels
+		BufferedImage image = new BufferedImage(matrix.cols(),matrix.rows(), type);
+		final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(buffer, 0, targetPixels, 0, buffer.length);  
 		return image;
-
 	}
 
 }
