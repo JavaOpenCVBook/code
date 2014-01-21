1209c45b456004b70fec0111b3c2c5bbe14baaa1
Daniel Lélis Baggio <danielbaggio@gmail.com>
2014-01-21 02:40:23 -0200

    Video playback class.

---
+++ chapter2/videoplayback/src/main/java/org/javaopencvbook/App.java
@@ -0,0 +1,66 @@
+package org.javaopencvbook;
+
+import java.awt.Image;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.javaopencvbook.utils.ImageProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
+public class App 
+{
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
+	}
+	
+	private JFrame frame;
+	private JLabel imageLabel;
+	
+	public static void main(String[] args) throws InterruptedException {
+		App app = new App();
+		app.initGUI();
+		app.runMainLoop(args);
+	}
+	
+	private void initGUI() {
+		frame = new JFrame("Video Playback Example");  
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		frame.setSize(400,400);  
+		imageLabel = new JLabel();
+		frame.add(imageLabel);
+		frame.setVisible(true);       
+	}
+
+	private void runMainLoop(String[] args) throws InterruptedException {
+		ImageProcessor imageProcessor = new ImageProcessor();
+		Mat webcamMatImage = new Mat();  
+		Image tempImage;  
+		VideoCapture capture = new VideoCapture("src/main/resources/videos/tree.avi");
+
+		if( capture.isOpened()){  
+			while (true){  
+				capture.read(webcamMatImage);  
+				if( !webcamMatImage.empty() ){  
+					tempImage= imageProcessor.toBufferedImage(webcamMatImage);
+					ImageIcon imageIcon = new ImageIcon(tempImage, "Video playback");
+					imageLabel.setIcon(imageIcon);
+					frame.pack();  //this will resize the window to fit the image
+					Thread.sleep(50);
+				}  
+				else{  
+					System.out.println(" Frame not captured or video has finished"); 
+					break;  
+				}
+			}  
+		}
+		else{
+			System.out.println("Couldn't open video file.");
+		}
+		
+	}
+}
