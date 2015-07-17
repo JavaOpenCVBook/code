12c12ce74e1dd2ef311bb785cfe2f27669f29cfc
Daniel Lélis Baggio <danielbaggio@gmail.com>
2015-07-17 02:56:18 -0300

    3.0.0
    Highgui changes.

--- chapter3/floodfill/src/main/java/org/javaopencvbook/App.java
+++ chapter3/floodfill/src/main/java/org/javaopencvbook/App.java
@@ -2,7 +2,7 @@ package org.javaopencvbook;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class App 
 {
@@ -10,7 +10,7 @@ public class App
 
 	public static void main(String[] args) throws Exception {
 		String filePath = "src/main/resources/images/cathedral-small.jpg";
-		Mat newImage = Highgui.imread(filePath);
+		Mat newImage = Imgcodecs.imread(filePath);
 
 		if(newImage.dataAddr()==0){
 			System.out.println("Couldn't open file " + filePath);
