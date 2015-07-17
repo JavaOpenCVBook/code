0a894dd18cbc2eabc43604c04de897386a851957
Daniel Lélis Baggio <danielbaggio@gmail.com>
2015-07-17 16:06:17 -0300

    upg 3.0.0

--- chapter3/threshold/src/main/java/org/javaopencvbook/App.java
+++ chapter3/threshold/src/main/java/org/javaopencvbook/App.java
@@ -1,12 +1,8 @@
 package org.javaopencvbook;
 
-import org.javaopencvbook.utils.ImageProcessor;
-import org.javaopencvbook.utils.ImageViewer;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
-import static org.opencv.imgproc.Imgproc.blur;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class App 
 {
@@ -14,7 +10,7 @@ public class App
 
 	public static void main(String[] args) throws Exception {
 		String filePath = "src/main/resources/images/apple.jpg";
-		Mat newImage = Highgui.imread(filePath, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+		Mat newImage = Imgcodecs.imread(filePath, Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
 
 		if(newImage.dataAddr()==0){
 			System.out.println("Couldn't open file " + filePath);
