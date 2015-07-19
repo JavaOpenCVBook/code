3178c1d456d451384bf1ed01e394623bd1964ff4
Daniel Lélis Baggio <danielbaggio@gmail.com>
2015-07-19 00:10:43 -0300

    Upgrade kernels to 3.0.0.
    Only highgui.

--- chapter4/kernels/src/main/java/org/javaopencvbook/App.java
+++ chapter4/kernels/src/main/java/org/javaopencvbook/App.java
@@ -1,11 +1,8 @@
 package org.javaopencvbook;
 
-import org.javaopencvbook.utils.ImageProcessor;
-import org.javaopencvbook.utils.ImageViewer;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.opencv.highgui.Highgui;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class App 
 {
@@ -13,7 +10,7 @@ public class App
 
 	public static void main(String[] args) throws Exception {
 		String filePath = "src/main/resources/images/clouds.png";
-		Mat newImage = Highgui.imread(filePath, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+		Mat newImage = Imgcodecs.imread(filePath, Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
 
 		if(newImage.dataAddr()==0){
 			System.out.println("Couldn't open file " + filePath);
