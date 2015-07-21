ec96169597898b25e8325c16c2687887e8d26fdc
Daniel Lélis Baggio <danielbaggio@gmail.com>
2015-07-21 00:58:11 -0300

    Upgrade to 3.0

--- chapter4/distance/src/main/java/org/javaopencvbook/App.java
+++ chapter4/distance/src/main/java/org/javaopencvbook/App.java
@@ -1,27 +1,17 @@
 package org.javaopencvbook;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class App 
 {
 	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
 	public static void main(String[] args) throws Exception {
-		Mat image = new Mat(3,3 ,CvType.CV_8UC1);
-		Mat sum = new Mat();
-		byte[] buffer = {0,2,4,6,8,10,12,14,16};
-		image.put(0,0,buffer);
-		System.out.println(image.dump());
-		Imgproc.integral(image, sum);
-		System.out.println(sum.dump());
-		
 		
 		String filePath = "src/main/resources/images/coffee.jpg";
-		Mat newImage = Highgui.imread(filePath, Highgui.CV_LOAD_IMAGE_ANYCOLOR);
+		Mat newImage = Imgcodecs.imread(filePath, Imgcodecs.CV_LOAD_IMAGE_ANYCOLOR);
 
 		if(newImage.dataAddr()==0){
 			System.out.println("Couldn't open file " + filePath);
